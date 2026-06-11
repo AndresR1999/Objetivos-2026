573d39818f5b89a4799b67300be1f5a5037df2a6
Andrés Rodrigáñez <andres.rodriganez@mango.com>
2026-06-11 06:34:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2043,7 +2043,7 @@ if seccion == "📊 Proveedores y tendencias":
             
                 st.markdown(
                     """
-                    <div style="height: 82px;"></div>
+                    <div style="height: 84px;"></div>
                     """,
                     unsafe_allow_html=True
                 )
